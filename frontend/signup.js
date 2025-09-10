424526e418d8e25693577b139659cfd7789f3280
bin <220984804+seobin-n-x@users.noreply.github.com>
2025-09-10 09:20:29 +0900

    회원 가입 로직 기능 구현
    프론트엔드 및 백엔드 작업(signup)

---
+++ frontend/signup.js
@@ -0,0 +1,37 @@
+const form = document.querySelector("#signup-form");
+
+const checkPassword = () => {
+  const formData = new FormData(form);
+  const password1 = formData.get("password");
+  const password2 = formData.get("password2");
+  if (password1 === password2) {
+    return true;
+  } else false;
+};
+
+const handleSubmit = async (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const sha256password = sha256(formData.get("password"));
+  formData.set("password", sha256password);
+
+  const div = document.querySelector("#info");
+
+  if (checkPassword()) {
+    const res = await fetch("/signup", {
+      method: "POST",
+      body: formData,
+    });
+    const data = await res.json();
+    if (data === "200") {
+      div.innerText = "회원가입에 성공";
+      div.style.color = "blue";
+      window.location.pathname = "/login.html";
+    }
+  } else {
+    div.innerText = "비밀번호가 같지 않습니다.";
+    div.style.color = "red";
+  }
+};
+
+form.addEventListener("submit", handleSubmit);
